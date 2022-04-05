43c6291513925325a0f4b147e06eccae5ea9c420
chirpthetwerp <32555127+chirpthetwerp@users.noreply.github.com>
2022-04-05 14:37:56 -0400

    added sql connection

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, request 
+
+from src.connection import db
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost:3306/test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+db.init_app(app)
 
 @app.get('/home')
 def home():
